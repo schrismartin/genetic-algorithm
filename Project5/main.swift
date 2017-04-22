928a4a561b6d8787b3aaf18af00c8d423017d0f1
Chris Martin <schrismartin@me.com>
2017-04-22 16:23:50 -0400

    Initial Commit

---
+++ Project5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project5
+//
+//  Created by Chris Martin on 4/22/17.
+//  Copyright © 2017 Chris Martin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
