04e706522e34af2e4fe333328c6b25bb4cde45fd
Chris Martin <schrismartin@me.com>
2017-04-24 18:52:37 -0400

    Initial commit

---
+++ Project5/Bit.swift
@@ -0,0 +1,9 @@
+//
+//  Bit.swift
+//  Project5
+//
+//  Created by Chris Martin on 4/22/17.
+//  Copyright © 2017 Chris Martin. All rights reserved.
+//
+
+import Foundation
---
+++ Project5/Bitstring.swift
@@ -0,0 +1,9 @@
+//
+//  Bitstring.swift
+//  Project5
+//
+//  Created by Chris Martin on 4/22/17.
+//  Copyright © 2017 Chris Martin. All rights reserved.
+//
+
+import Foundation
